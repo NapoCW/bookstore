959d56e8f66481ca89332970581919b59a3d9478
nvarelas <n.varelas@acg.edu>
2024-12-09 23:10:24 +0200

    whitenoise

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 LOGOUT_REDIRECT_URL = 'home'
 
