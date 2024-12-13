723dec612550d0701c0810f5ba6c538dc1bffc76
nvarelas <n.varelas@acg.edu>
2024-12-13 22:08:59 +0200

    settings fix

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY','django-insecure-lk=egau#8uogvkg!!_4+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
